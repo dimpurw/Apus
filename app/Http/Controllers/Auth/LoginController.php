a292c589ed4719936254caab3aa24fe3271c4787
Dimas Pratama Purwadinata <54875915+dimpurw@users.noreply.github.com>
2021-01-24 22:04:32 +0700

    new auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,29 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    public function postlogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('/home');
+        }
+
+        return redirect('/')->with('error', 'Oppes! You have entered invalid credentials');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/')->with('logout', 'Anda yakin ingin keluar ?');
+    }
 }
