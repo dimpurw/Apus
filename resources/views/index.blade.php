39ad0de06cb4dbd2e5d92bc13dadb57aee22a692
Dimas Pratama Purwadinata <54875915+dimpurw@users.noreply.github.com>
2021-01-25 20:28:24 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,36 +2,6 @@
 
 @section('content')
 
-<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="login" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Form Login</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <form method="POST" action="postlogin">
-        @csrf
-        <div class="modal-body">
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Email:</label>
-            <input type="email" name="email" class="form-control" id="email">
-          </div>
-          <div class="form-group">
-            <label for="message-text" class="col-form-label">Password:</label>
-            <input type="password" name="password" class="form-control" id="password"></input>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Login</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-
 <div class="page-hero bg-image overlay-dark" style="background-image: url(assets/img/bg-apus.png);">
   <div class="hero-section">
     <div class="container text-center wow zoomIn">
