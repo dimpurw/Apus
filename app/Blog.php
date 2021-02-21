ec19d4e1e76acf4792798b599d76f4d114e32fc6
dimpurw <terminatorid2@gmail.com>
2021-02-21 14:29:05 +0700

    update model

--- app/Blog.php
+++ app/Blog.php
@@ -9,13 +9,4 @@ class Blog extends Model
     protected $fillable = [
         'title', 'description', 'foto'
     ];
-
-    public function blogFoto()
-    {
-        if (!$this->foto) {
-            return asset('dashboard/dist/img/user1-128x128.jpg');
-        }
-        return asset('blog/' . $this->foto);
-    }
-    
 }
