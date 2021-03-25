31aa45c22fe838c6f886961f57725cb95d247953
dimpurw <terminatorid2@gmail.com>
2021-03-25 21:27:51 +0700

    update model

--- app/RecapData.php
+++ app/RecapData.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class RecapData extends Model
 {
     protected $table = 'recap_datas';
+    protected $fillable = [
+        'nama_kucing', 'jenis_penyakit', 'presentase',
+    ];
 }
