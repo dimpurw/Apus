4fdb2e5b448d17753f075e3ee6cf9e4bfad0d9d7
dimpurw <terminatorid2@gmail.com>
2021-03-29 23:09:12 +0700

    update

--- resources/views/layouts-dashboard/app.blade.php
+++ resources/views/layouts-dashboard/app.blade.php
@@ -29,6 +29,7 @@
     <!-- Datatables -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.dataTables.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -86,6 +87,12 @@
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
     <script>
     $(function () {
         /*
@@ -144,10 +151,29 @@
         + Math.round(series.percent) + '%</div>'
     }
 </script>
-<script
->$(document).ready(function() {
-    $('#example2').DataTable();
-} );
+<script>
+$(document).ready(function() {
+    $('#example').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'excel',
+                exportOptions: 
+                {columns: [ 0, 1, 2]},
+            },
+            {
+                extend: 'pdf',
+                exportOptions: 
+                {columns: [ 0, 1, 2]},
+            },
+            {
+                extend: 'print',
+                exportOptions: 
+                {columns: [ 0, 1, 2]}
+            }
+        ]
+    });
+});
 </script>
 </body>
 
